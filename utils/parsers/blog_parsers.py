b427afaafa5e8dafc1f362037932a6895874309f
YahyaFazlani <yahyafazlani@outlook.com>
2021-06-05 00:11:12 +0530

    organized parsers

---
+++ utils/parsers/blog_parsers.py
@@ -0,0 +1,15 @@
+from flask_restful import reqparse
+from werkzeug.datastructures import FileStorage
+
+blog_create_parser = reqparse.RequestParser(bundle_errors=True)
+blog_create_parser.add_argument("title", type=str, required=True)
+blog_create_parser.add_argument("content", type=str, required=True)
+blog_create_parser.add_argument(
+    "thumbail", type=FileStorage, location="files", required=True)
+blog_create_parser.add_argument("is_published", type=bool, required=False)
+
+blog_update_parser = blog_create_parser.copy()
+blog_update_parser.replace_argument("title", required=False)
+blog_update_parser.replace_argument("content", required=False)
+blog_create_parser.replace_argument("thumbail", required=False)
+blog_update_parser.replace_argument("is_published", required=False)
---
+++ utils/parsers/user_parsers.py
@@ -0,0 +1,8 @@
+from flask_restful import reqparse
+from utils.email import validate_email
+
+user_create_parser = reqparse.RequestParser()
+user_create_parser.add_argument("firstname", type=str, required=True)
+user_create_parser.add_argument("lastname", type=str, required=True)
+user_create_parser.add_argument("email", type=validate_email, required=True)
+user_create_parser.add_argument("password", type=str, required=True)
