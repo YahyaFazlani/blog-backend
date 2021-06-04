9537c0290d5567432677150e0e6305a32f5ba8d9
YahyaFazlani <yahyafazlani@outlook.com>
2021-06-05 00:11:31 +0530

    updated

--- views/parsers/user_parsers.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import reqparse
-
-user_create_parser = reqparse.RequestParser()
-user_create_parser.add_argument("firstname", type=str, required=True)
-user_create_parser.add_argument("lastname", type=str, required=True)
-user_create_parser.add_argument("email", type=str, required=True)
-user_create_parser.add_argument("password", type=str, required=True)
