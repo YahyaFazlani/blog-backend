efd0edbfeb77f94fc6fef48bd8eb3da227c566a4
YahyaFazlani <yahyafazlani@outlook.com>
2021-06-06 16:12:46 +0530

    corrected import

--- utils/parsers/user_parsers.py
+++ utils/parsers/user_parsers.py
@@ -1,5 +1,5 @@
 from flask_restful import reqparse
-from utils.email import validate_email
+from utils.validate import validate_email
 
 user_create_parser = reqparse.RequestParser()
 user_create_parser.add_argument("firstname", type=str, required=True)
