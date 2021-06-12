cc3ae70c1d598eb4feca2d14b7f9dd0074c7aef8
YahyaFazlani <yahyafazlani@outlook.com>
2021-06-12 15:18:16 +0530

    better configurations

--- configs.py
+++ configs.py
@@ -1,15 +1,22 @@
+import os
+from datetime import timedelta
+
+
 class Config(object):
   TESTING = False
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   UPLOAD_FOLDER = "./media/thumbnails"
-  ALLOWED_EXTENSIONS = {"jpeg", "jpg", "png", "gif"}
-  SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+  ALLOWED_EXTENSIONS = tuple({".jpeg", ".jpg", ".png", ".gif"})
+  SECRET_KEY = os.environ.get("SECRET_KEY")
+  JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+  JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 
 
 class DevelopmentConfig(Config):
-  pass
+  SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
 
 
 class TestingConfig(Config):
   TESTING = True
   SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+  SECRET_KEY = "fjdsajfksd"
