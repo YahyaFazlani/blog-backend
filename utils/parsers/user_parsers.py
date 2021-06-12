babc2b628d713edec093c46d93800d16aad8cf4f
YahyaFazlani <yahyafazlani@outlook.com>
2021-06-12 15:20:35 +0530

    new_parser

--- utils/parsers/user_parsers.py
+++ utils/parsers/user_parsers.py
@@ -1,8 +1,12 @@
 from flask_restful import reqparse
 from utils.validate import validate_email
 
-user_create_parser = reqparse.RequestParser()
-user_create_parser.add_argument("firstname", type=str, required=True)
-user_create_parser.add_argument("lastname", type=str, required=True)
-user_create_parser.add_argument("email", type=validate_email, required=True)
-user_create_parser.add_argument("password", type=str, required=True)
+user_register_parser = reqparse.RequestParser()
+user_register_parser.add_argument("firstname", type=str, required=True)
+user_register_parser.add_argument("lastname", type=str, required=True)
+user_register_parser.add_argument("email", type=validate_email, required=True)
+user_register_parser.add_argument("password", type=str, required=True)
+
+user_login_parser = user_register_parser.copy()
+user_login_parser.remove_argument("firstname")
+user_login_parser.remove_argument("lastname")
