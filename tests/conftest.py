847d369c3b7e3fc5cc3782835e43e9e6f50cae8c
YahyaFazlani <yahyafazlani@outlook.com>
2021-06-12 15:18:28 +0530

    moved all auth action

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,46 @@
+from flask.testing import FlaskClient
 import pytest
 from app import create_app
 from models import db
-from models.blog import Blog
-from models.user import User
 
 
-@pytest.fixture(scope="module")
+class AuthActions(object):
+  def __init__(self, client: FlaskClient) -> None:
+    self._client = client
+
+  def register(self, firstname="Test", lastname="User", email="testuser@test.com", password="supersecret"):
+    data = {
+        "firstname": firstname,
+        "lastname": lastname,
+        "email": email,
+        "password": password
+    }
+
+    response = self._client.post("/auth/register", data=data)
+
+    return response
+
+  def login(self, email="testuser@test.com", password="supersecret"):
+    data = {
+        "email": email,
+        "password": password
+    }
+
+    response = self._client.post("/auth/login", data=data)
+
+    return response
+
+
+@pytest.fixture(scope="package")
 def client():
-  app = create_app
-  app.testing = True
-  app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
-  db.init_app(app)
+  app = create_app(config="configs.TestingConfig")
 
   with app.test_client() as client:
     with app.app_context():
       db.create_all()
-
-      for i in range(1, 5):
-        test_user = User(firstname=f"TestUser{i}", lastname="Blogger",
-                         email=f"testuser{i}@outlook.com", password="jfkdlsajfklds")
-        db.session.add(test_user)
-        db.session.commit()
     yield client
+
+
+@pytest.fixture(scope="module")
+def auth(client):
+  return AuthActions(client)
