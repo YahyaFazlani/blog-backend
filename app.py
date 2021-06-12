a3492e0ba226fc1366151490c8676136d4630cab
YahyaFazlani <yahyafazlani@outlook.com>
2021-06-12 15:35:59 +0530

    changed medai blueprint to view, formatted

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from models import db, migrate
 from views.auth import auth_bp, jwt
 from views.blog import blog_bp
 from views.errorhandlers import error_bp
-from views.media import media_bp
+from views.media import media
 
 
-def create_app(config = "configs.DevelopmentConfig"):
+def create_app(config="configs.DevelopmentConfig"):
   app = Flask(__name__)
   app.config.from_object(config)
+
+  app.add_url_rule("/media/<path:path>", view_func=media)
 
   db.init_app(app)
   jwt.init_app(app)
@@ -20,6 +22,5 @@ def create_app(config = "configs.DevelopmentConfig"):
   app.register_blueprint(blog_bp)
   app.register_blueprint(auth_bp)
   app.register_blueprint(error_bp)
-  app.register_blueprint(media_bp)
 
   return app
