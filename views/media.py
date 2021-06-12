4cd6081e6bf2eda1a818d5dbbf27fed789ecc6be
YahyaFazlani <yahyafazlani@outlook.com>
2021-06-12 15:35:25 +0530

    simplified media endpoint

--- views/media.py
+++ views/media.py
@@ -1,8 +1,5 @@
-from flask import Blueprint, send_from_directory
-
-media_bp = Blueprint("media", __name__, url_prefix="/media/")
+from flask import send_from_directory
 
 
-@media_bp.route("/<path:path>")
 def media(path):
   return send_from_directory("media", path)
